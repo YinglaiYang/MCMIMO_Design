2ee017434b150f06f2fb8a54b97714697953472b
Yinglai Yang <yinglai@Yinglais-MBP.fritz.box>
2016-07-11 11:17:39 +0200

    Refactoring

--- src/cfun_template.m
+++ src/cfun_template.m
@@ -1,11 +1,11 @@
-function [ costVal ] = cfun( x )
+function [ costVal ] = cfun_template( x, env )
 % Cost function (CRB of system)
 % Assumptions:
 % - power identical for all frequencies
 % - power identical for all antenna positions
 % - p_x is centered to 0 by this function
 
-[ p,f ] = disassembleX( x );
+[ p,f ] = disassembleX( x, env );
 
 
 costVal= 1/(f'*f) / var(p,1);
