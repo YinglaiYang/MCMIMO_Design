369a50b9b885bd8f284863b29451f5945d9391d2
Yinglai Yang <yinglai@s-8d3a339b.on.site.uni-stuttgart.de>
2016-07-28 15:00:47 +0200

    Illustrations

--- illustrations/compare_MC_SC.m
+++ illustrations/compare_MC_SC.m
@@ -3,26 +3,42 @@ clc, clear, close all;
 N_ux = 1024;
 N_r  = 91;
 
+SNR_dB = 10;
+
+kappa = getKappa(SNR_dB);
+
 %% First load any data.
-mc = load('/Users/yinglai/GitHub/MCMIMO_Design/results/15-07-2016/01_workspace_reducedUniversalGA.mat');
-sc = load('/Users/yinglai/GitHub/MCMIMO_Design/results/15-07-2016/02_workspace_reducedUniversalGA_SC.mat');
+mc = load('/Users/yinglai/GitHub/MCMIMO_Design/results/21-07-2016/MC_workspace_reducedUniversalGA_MC.mat');
+sc = load('/Users/yinglai/GitHub/MCMIMO_Design/results/21-07-2016/SC_workspace_reducedUniversalGA_SC.mat');
 
 % MC
-[px,fc] = disassembleX(mc.p_opt,mc.env);
-ap = arrayPattern(px-mean(px),fc,N_ux,N_r);
+[mc.px,mc.fc] = disassembleX(mc.p_opt,mc.env);
+mc.px=(mc.px-mean(mc.px))*1.1;
+mc.ap = arrayPattern(mc.px-mean(mc.px),mc.fc,N_ux,N_r);
 
-r_max = mc.c/(max(fc)-min(fc));
+r_max = mc.c/(max(mc.fc)-min(mc.fc));
 
 % The ROI for ux and r
 ux = linspace(-1,1,N_ux);
-r  = linspace(-r_max/2, r_max/2, N_r);
+r  = linspace(-0.5,0.5, N_r);
 
-figure, imagesc(ux,r,ap);
+figure, imagesc(ux,r,mc.ap);
 
-figure, plot(ux,ap(1,:));
+figure, plot(ux,mc.ap(1,:));
+
+CRB_mc = kappa * cfun_pf(mc.px,mc.fc)
+sll_mc = getSLL(mc.px,mc.fc,0.2)
 
 % SC
-[px,fc] = disassembleX(sc.p_opt,sc.env);
-ap = arrayPattern(px-mean(px),fc,N_ux,1);
+[sc.px,sc.fc] = disassembleX(sc.p_opt,sc.env);
+sc.px=(sc.px-mean(sc.px))*1.0;
+%sc.fc=[sc.fc;sc.fc];
+sc.ap = arrayPattern(sc.px-mean(sc.px),sc.fc,N_ux,1);
 
-figure, plot(ux,ap);
+figure, plot(ux,sc.ap);
+
+CRB_sc = kappa * cfun_pf(sc.px,sc.fc) /2
+sll_sc = getSLL(sc.px,sc.fc,0.2)
+
+%% Combination
+figure, plot(ux,mc.ap(1,:), ux,sc.ap);
